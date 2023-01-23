938dabd7a0ef79c2c1b4fc7efcc2a21681f55b1a
ArturMand <108627874+ArturMand@users.noreply.github.com>
2023-01-23 21:46:40 +0000

    Deploying to gh-pages from @ ArturMand/window@5fdd1c9d51ddbca8db564bda0e24eaa2cfe4c2d5 🚀

---
+++ index.d3e12462.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");e.addEventListener("click",(()=>{e.classList.toggle("is-open"),t.classList.toggle("is-open")}))})();
+//# sourceMappingURL=index.d3e12462.js.map
