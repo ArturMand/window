5fdd1c9d51ddbca8db564bda0e24eaa2cfe4c2d5
ArturMand <ArturMandryk@gmail.com>
2023-01-23 23:38:11 +0200

    aa

--- src/js/header/mobileMenu/mobileMenu.js
+++ src/js/header/mobileMenu/mobileMenu.js
@@ -1,17 +1,9 @@
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
-  const navRef = document.querySelector('.nav');
   menuBtnRef.addEventListener('click', () => {
     menuBtnRef.classList.toggle('is-open');
     mobileMenuRef.classList.toggle('is-open');
-    // const unScroll = mobileMenuRef.classList.contains('is-open')
-    // if(unScroll){
-    //   document.body.style.overflowY='scroll'
-    //   document.body.style.position='fixed'
-    //   // document.body.style.overflowY='scroll'
-    // }else{
-    //   document.body.style.overflow='auto'
-    // }
+
   });
 })();
